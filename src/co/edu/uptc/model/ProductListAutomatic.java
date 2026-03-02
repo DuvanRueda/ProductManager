096fabe32030c44f788db2a2a8b2c94faf359e96
Juan Ch. <juan.chaparro21@uptc.edu.co>
2026-03-02 13:31:25 -0500

    Second version of ProductList implemented

--- src/co/edu/uptc/model/ProductListAutomatic.java
+++ src/co/edu/uptc/model/ProductListAutomatic.java
@@ -2,11 +2,22 @@ package co.edu.uptc.model;
 
 import co.edu.uptc.interfaces.ProductListInterface;
 import co.edu.uptc.pojo.Product;
+import co.edu.uptc.util.Constants;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-public class ProductListAutomatic implements ProductListInterface {
+public class ProductListAutomatic implements ProductListInterface{
+    ArrayList<Product> list = new ArrayList<>();
+
     @Override
     public Product createProduct(String value) {
-        return null;
+        String[] values = value.split(" ");
+        String name = values[0];
+        String description = values[1];
+        String measurementType = values[2];
+        int price = Integer.parseInt(values[3]);
+        return new Product(name, description, measurementType, price);
+         
     }
 
     @Override
@@ -16,28 +27,43 @@ public class ProductListAutomatic implements ProductListInterface {
 
     @Override
     public void addEnd(String value) {
-
+        Product aux = createProduct(value);
+        list.addLast(aux);
     }
 
     @Override
     public String deleteProduct(String key) {
-
-        return null;
+        for (Product product : list) {
+            if (product.getName().contains(key)) {
+                list.remove(list.indexOf(product));
+            }
+        }
+        return Constants.SUCCESSFULLY_REMOVED;
     }
 
     @Override
     public Product[] sortList() {
-        return null;
+        ArrayList<Product> aux = list;
+        aux.sort(Comparator.comparing(Product::getName));
+        return aux.toArray(new Product[0]);
     }
 
     @Override
-    public String showInfo() {
-        return "";
+    public String showInfo(){
+        if(returnLength()<=0){
+            return Constants.SIZE_ERROR;
+        }
+        Product[] aux = sortList();
+        
+        String info = "";
+        for (int i = 0; i < returnLength(); i++) {
+            info += aux[i].getName()+"   "+ aux[i].getDescription()+"   "+aux[i].getMeasurementType()+"   "+ aux[i].getPrice()+"\n";
+        }
+        return info;
     }
 
     @Override
     public int returnLength() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'returnLength'");
+        return list.size();
     }
 }
--- src/co/edu/uptc/util/Constants.java
+++ src/co/edu/uptc/util/Constants.java
@@ -3,19 +3,20 @@ package co.edu.uptc.util;
 public class Constants {
 
     //Mensajes de Usuario
-    public static String STR_MENU = "Escriba el número que acompaña la acción: \n 1.Listar Productos\n 2.Agregar Producto\n 3.Eliminar Producto\n 4.Salir";
-    public static String CLOSE_APP = "Aplicación cerrada.";
-    public static String SUCCESSFUL_PROCESS = "¡Proceso exitoso!";
-    public static String ENTRANCE_EXAMPLE = "Ingrese la nombre, descipción, tipo de unidad y su valor. Ej: Manzana fruta unidad 1000";
-    public static String DELETE_PRODUCT = "Ingrese el del producto que desea eliminar.";
-    public static String NON_PRODUCT = "Producto no encontrado.";
-    public static String SUCCESSFULLY_REMOVED = "Producto eliminado con exito.";
+    public static final String STR_MENU = "Escriba el número que acompaña la acción: \n 1.Listar Productos\n 2.Agregar Producto\n 3.Eliminar Producto\n 4.Salir";
+    public static final String CLOSE_APP = "Aplicación cerrada.";
+    public static final String SUCCESSFUL_PROCESS = "¡Proceso exitoso!";
+    public static final String ENTRANCE_EXAMPLE = "Ingrese la nombre, descipción, tipo de unidad y su valor. Ej: Manzana fruta unidad 1000";
+    public static final String DELETE_PRODUCT = "Ingrese el del producto que desea eliminar.";
+    public static final String NON_PRODUCT = "Producto no encontrado.";
+    public static final String SUCCESSFULLY_REMOVED = "Producto eliminado con exito.";
 
     //Errores
-    public static String TYPING_ERROR = "Dato mal digitado.";
+    public static final String TYPING_ERROR = "Dato mal digitado.";
+    public static final String SIZE_ERROR = "La lista no contiene datos";
 
     //Palabras sueltas
-    public static String NAME = "Nombre: ";
-    public static String DESCRIPTION = "Descripción: ";
-    public static String PRICE = "Precio: ";
+    public static final String NAME = "Nombre: ";
+    public static final String DESCRIPTION = "Descripción: ";
+    public static final String PRICE = "Precio: ";
 }
